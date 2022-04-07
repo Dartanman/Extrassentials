b563301212b6b149f238b1a4354ad6fe6eac526b
Austin Dart <aedart@iastate.edu>
2022-04-07 13:14:16 -0500

    Manage EPlayers on join and quit

--- Extrassentials/src/main/java/me/dartanman/extrassentials/core/Extrassentials.java
+++ Extrassentials/src/main/java/me/dartanman/extrassentials/core/Extrassentials.java
@@ -2,6 +2,7 @@ package me.dartanman.extrassentials.core;
 
 import me.dartanman.extrassentials.core.commands.ExtrassentialsCommand;
 import me.dartanman.extrassentials.core.files.FileManager;
+import me.dartanman.extrassentials.core.listeners.EPlayerListener;
 import me.dartanman.extrassentials.join.listeners.JoinListener;
 import me.dartanman.extrassentials.join.listeners.QuitListener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,8 +26,16 @@ public class Extrassentials extends JavaPlugin
 	 */
 	private void enableCore()
 	{
+		// Setup
 		FileManager.init();
+
+		// Events
+		getServer().getPluginManager().registerEvents(new EPlayerListener(), this);
+
+		// Commands
 		getCommand("extrassentials").setExecutor(new ExtrassentialsCommand());
+
+		// Info
 		getLogger().info("Extrassentials-Core has been enabled. The core cannot be disabled (except by uninstalling Extrassentials)");
 	}
 
@@ -35,16 +44,24 @@ public class Extrassentials extends JavaPlugin
 	 */
 	private void enableExtrassentialsJoin()
 	{
+		// Setup
 		boolean enabled = FileManager.getConfig().getBoolean("Extrassentials-Join.Enabled");
 
 		if(enabled)
 		{
+			// Events
 			getServer().getPluginManager().registerEvents(new JoinListener(), this);
 			getServer().getPluginManager().registerEvents(new QuitListener(), this);
-			getLogger().info("Extrassentials-Join has been enabled.");
+
+			// Commands
+			// (there are none right now)
+
+			// Info
+			getLogger().info("Extrassentials-Join has been enabled. You may disable it in the Extrassentials config.yml");
 		}
 		else
 		{
+			// Info
 			getLogger().info("Extrassentials-Join has not been enabled. If you wish to enable this module, do so in the Extrassentials config.yml");
 		}
 	}
