12cea655b920cf26566b153b5caa7e3eed7d9506
Austin Dart <aedart@iastate.edu>
2022-04-06 20:13:29 -0500

    Create EPlayer management; add JavaDoc comments

--- Extrassentials/src/main/java/me/dartanman/extrassentials/players/EPlayer.java
+++ Extrassentials/src/main/java/me/dartanman/extrassentials/players/EPlayer.java
@@ -17,17 +17,32 @@ public class EPlayer
 	
 	private Player player;
 	
+	/**
+	 * EPlayer constructor
+	 * @param player
+	 * 		The Player that this EPlayer wraps
+	 */
 	public EPlayer(Player player)
 	{
 		this.player = player;
 		this.uuid = player.getUniqueId();
 	}
 	
+	/**
+	 * Gets the UUID of the EPlayer -- it is an exact copy of the Player's UUID
+	 * @return
+	 * 		The UUID of the EPlayer
+	 */
 	public UUID getUniqueId()
 	{
 		return uuid;
 	}
 	
+	/**
+	 * Sends a colored message. See me.dartanman.extrassentials.utils.StringUtils.colorize(String) to understand how it colors the message
+	 * @param message
+	 * 		The message to color and send
+	 */
 	public void sendColoredMessage(String message)
 	{
 		message = StringUtils.colorize(message);
---
+++ Extrassentials/src/main/java/me/dartanman/extrassentials/players/EPlayerManager.java
@@ -0,0 +1,77 @@
+package me.dartanman.extrassentials.players;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * EPlayerManager -- Manages EPlayer objects
+ * @author Austin Dart (Dartanman)
+ */
+public class EPlayerManager 
+{
+	
+	private List<EPlayer> ePlayerList;
+	
+	/**
+	 * EPlayerManager constructor
+	 */
+	public EPlayerManager()
+	{
+		ePlayerList = new ArrayList<>();
+	}
+	
+	/**
+	 * Get a list of all the EPlayers
+	 * @return
+	 * 		List of EPlayers
+	 */
+	public List<EPlayer> getEPlayerList()
+	{
+		return ePlayerList;
+	}
+	
+	/**
+	 * Add an EPlayer to the list
+	 * @param ePlayer
+	 * 		EPlayer to add
+	 * @return
+	 * 		The boolean value of List.add()
+	 */
+	public boolean addEPlayer(EPlayer ePlayer)
+	{
+		return ePlayerList.add(ePlayer);
+	}
+	
+	/**
+	 * Remove an EPlayer from the list
+	 * @param ePlayer
+	 * 		EPlayer to remove
+	 * @return
+	 * 		The boolean value of List.remove()
+	 */
+	public boolean removeEPlayer(EPlayer ePlayer)
+	{
+		return ePlayerList.remove(ePlayer);
+	}
+	
+	/**
+	 * Get an EPlayer based off of the UUID
+	 * @param uuid
+	 * 		The UUID
+	 * @return
+	 * 		The EPlayer, or null if not found
+	 */
+	public EPlayer getByUUID(UUID uuid)
+	{
+		for(EPlayer ePlayer : ePlayerList)
+		{
+			if(ePlayer.getUniqueId().equals(uuid))
+			{
+				return ePlayer;
+			}
+		}
+		return null;
+	}
+
+}
