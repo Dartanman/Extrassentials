a4dbab1f65f9a4e3e10ac115b3880bfaa124be78
Austin Dart <aedart@iastate.edu>
2022-04-06 19:38:55 -0500

    Add barebone requirements to make a plugin

---
+++ Extrassentials/src/main/java/me/dartanman/extrassentials/Extrassentials.java
@@ -0,0 +1,17 @@
+package me.dartanman.extrassentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Extrassentials Main Class
+ * @author Austin Dart (Dartanman)
+ */
+public class Extrassentials extends JavaPlugin
+{
+	
+	public void onEnable()
+	{
+		// This will enable everything in the plugin
+	}
+
+}
