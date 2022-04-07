c4c64c6be08f86cb00cfb669f6cc7b2b9ceaafe8
Austin Dart <aedart@iastate.edu>
2022-04-06 21:23:15 -0500

    Add silent joins

--- Extrassentials/src/main/java/me/dartanman/extrassentials/join/listeners/JoinListener.java
+++ Extrassentials/src/main/java/me/dartanman/extrassentials/join/listeners/JoinListener.java
@@ -2,6 +2,8 @@ package me.dartanman.extrassentials.join.listeners;
 
 import me.dartanman.extrassentials.core.files.FileManager;
 import me.dartanman.extrassentials.core.utils.StringUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,9 +14,22 @@ public class JoinListener implements Listener
     @EventHandler
     public void onJoin(PlayerJoinEvent event)
     {
-        String joinMessage = StringUtils.colorize(FileManager.getConfig().getString("Extrassentials-Join.Custom-Join-Message"));
-        joinMessage = joinMessage.replace("<player>", event.getPlayer().getName());
+        FileConfiguration config = FileManager.getConfig();
+        Player player = event.getPlayer();
+        String joinMessage = StringUtils.colorize(config.getString("Extrassentials-Join.Custom-Join-Message"));
+        joinMessage = joinMessage.replace("<player>", player.getName());
         event.setJoinMessage(joinMessage);
+
+        boolean silentJoinEnabled = config.getBoolean("Extrassentials-Join.Silent-Join.Enabled");
+        if(silentJoinEnabled)
+        {
+            String silentPermission = config.getString("Extrassentials-Join.Silent-Join.Permission");
+            assert silentPermission != null;
+            if(player.hasPermission(silentPermission))
+            {
+                event.setJoinMessage(null);
+            }
+        }
     }
 
 }
